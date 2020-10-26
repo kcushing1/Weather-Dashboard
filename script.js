cc5c2a13d6eb824f61f1ffadfc2303ba3116f5d5
Kasey Cushing <kasey.cushing@gmail.com>
2020-10-26 19:28:29 -0400

    UV index API functional, UV displays with appropriate index color

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ function searchCity(){
 
   //clear card
 
-     //queryURL template for OpenWeather API
+     //send for OpenWeather API for today's weather
      let city = $(".search-city").val()
      let queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=imperial&appid=349bd553f59e26c071b517009066832a";
      let forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city +"&units=imperial&appid=349bd553f59e26c071b517009066832a";
+
      let todayLong = moment().format("LL")
      let today = moment()
-
-     console.log(city)
 
      $.ajax({
        url: queryURL,
@@ -24,15 +23,14 @@ function searchCity(){
      }).then(function(response) {
        console.log(response);
        let results = response.main
-       //city (date) + icon
        let cityName = response.name
        //let cityIcon = response.weather[0].icon
+
        //add city name as header with date & icon
-       $(".current-city").html(`<h3> ${cityName} ${todayLong} icon </h3>`)
+       $(".current-city").html(`<h3> ${cityName} - ${todayLong} icon </h3>`)
 
        //add temp 
-       let cityTemp = results.temp
-       //((results.temp - 273.15) * 1.8 + 32).toFixed(1)
+       let cityTemp = results.temp.toFixed(1)
        $(".current-temp").text(`Temperature: ${cityTemp} ˚F`)
        // add humidity
        let cityHumidity = results.humidity
@@ -40,22 +38,58 @@ function searchCity(){
        // add wind speed
        let cityWind = response.wind.speed
        $(".current-wind").text(`Wind Speed: ${cityWind} mph`)
-       //uv index...can't locate?
 
-     }); //end ajax current
+       cityUV()
+      //need to use fist ajax response to get coords
+      //need coords to get UV, can't do by city name
+       function cityUV(){
+        let cityLat = response.coord.lat
+        let cityLon = response.coord.lon
+        let ultraVURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + cityLat + "&lon=" + cityLon + "&appid=349bd553f59e26c071b517009066832a"
+  
+          $.ajax({
+            url: ultraVURL,
+            method: "GET"
+          }).then(function(UV) {
+            console.log(UV)
+            uvIndex = UV.value
+            $(".current-uv").html(`<p>UV Index: 
+            <span class="uv rounded">${uvIndex}<span>
+            </p>`)
+            colorUVIndex()
+
+            //indicate index severity
+            function colorUVIndex(){
+              if (uvIndex < 3){
+                $(".uv").addClass("low-uv")
+              } else if (uvIndex >= 3 || uvIndex < 6){
+                 $(".uv").addClass("mod-uv")
+              } else if (uvIndex >= 6 || uvIndex <8){
+                $(".uv").addClass("high-uv")
+              } else if (uvIndex >= 8 || uvIndex < 10){
+                $(".uv").addClass("very-high-uv")
+              } else {
+                $(".uv").addClass("extreme-uv")
+              }
+            }
+          })//end UV ajax
+        } //end cityUV ftn
+
+     })//end ajax current
+
 
      cityForecast()
 
+     //send for OpenWeather API for forecasted weather
+     //returns forecast in 3hr increments for 5 days
      function cityForecast(){
       $.ajax({
         url: forecastURL,
         method: "GET"
       }).then(function(future) {
         console.log(future)
-        //time is 3hr incr for 5 days
-        //need to check time for 12:00:00 in str
-        //future.list[i].dt_txt is 
-        //str "2020-10-27 12:00:00
+
+        //make array to capture relevent points in API object response
         let futureArray = []
         
         getNoontime()
@@ -67,15 +101,16 @@ function searchCity(){
             let noontime = "12:00:00"
 
             //if the time is noon and the day is not today
-            //add that i to futureArray
+            //check if string noontime is present
+            //add that i value to futureArray
             if (checkFutureTime.includes(noontime) && checkFutureTime !== today){
-              console.log("checkfuturetime is connected")
               futureArray.push(i)
             }
           }
           console.log(futureArray)
         }
 
+        //add the forecast below the present weather
         addForecastCards()
 
         function addForecastCards(){
@@ -89,7 +124,7 @@ function searchCity(){
           
            //create the card with title, icon, temp, hum.
            $(".card-holder").append(`
-            <div class="col card">
+            <div class="col card rounded">
               <div class="card-body p-0">
                 <h6 class="card-title">${tomorrow.format("l")}</h6>
                 <p> icon </p>
@@ -99,16 +134,12 @@ function searchCity(){
             </div>
            `);
 
-            //add a day to the display date on next card
+            //add a day to the display date for the next card
             tomorrow.add(1,"day")
           }
           console.log("addforecastcards is connected")
         }
-
-        //for moment loop start tomorrow, add i day, append 
-       
-        
-        
+  
         //icon .weather[0].main = "clouds"
         //if statement for chosing icon?
         //Clouds, Rain
@@ -117,11 +148,12 @@ function searchCity(){
      //prev cities searched
      //log as buttons? cards?
       }) //end forecast ajax
-     } //end cityForecast()
-     
+     } //end cityForecast()    
 
   console.log("search btn connected")
-}
+}//end search btn
 
 
 //prev-city search click event...maybe?
+//add event, in function(){
+  // set $(".search-city").val($this) or something
